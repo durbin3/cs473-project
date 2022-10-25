ef367335873b5e8b6400196cc98d9fe4bb3a969d
durbin3 <54672526+durbin3@users.noreply.github.com>
2022-10-25 10:47:10 -0400

    Add resized images and partial labels

---
+++ scripts/image_resizing.py
@@ -0,0 +1,34 @@
+from PIL import ImageOps,Image
+import os
+
+
+
+def main():
+    size = (1000,1000)
+    directory = '../raw_images'
+    out_dir = '../resized_images'
+    if not os.path.exists(out_dir): os.makedirs(out_dir)
+
+    for file in os.listdir(directory):
+        if file.endswith(('jpeg', 'png', 'jpg')):
+            outfile = os.path.join(out_dir, file)
+            with Image.open(directory+'/'+file) as im:
+                square = expand2square(im)
+                out = square.resize(size,resample=Image.Resampling.LANCZOS)
+                out.save(outfile,quality=100)
+
+def expand2square(image):
+    width, height = image.size
+    if width == height:
+        return image
+    elif width > height:
+        result = Image.new(image.mode, (width, width), (255,255,255))
+        result.paste(image, (0, (width - height) // 2))
+        return result
+    else:
+        result = Image.new(image.mode, (height, height), (255,255,255))
+        result.paste(image, ((height - width) // 2, 0))
+        return result
+
+
+if __name__ == '__main__': main()
