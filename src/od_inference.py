b87b184971edf181755a0e917fcfcc815136396d
Eric Durbin <ericdurbinf@gmail.com>
2022-11-02 13:42:02 -0400

    Modify Inference to accept raw image inputs

--- src/od_inference.py
+++ src/od_inference.py
@@ -1,5 +1,4 @@
 import os
-import pathlib
 import tensorflow as tf
 import numpy as np
 from PIL import Image
@@ -15,42 +14,51 @@ tf.get_logger().setLevel('ERROR')           # Suppress TensorFlow logging (2)
 warnings.filterwarnings('ignore')   # Suppress Matplotlib warnings
 
 # Enable GPU dynamic memory allocation
-gpus = tf.config.experimental.list_physical_devices('GPU')
-for gpu in gpus:
-    tf.config.experimental.set_memory_growth(gpu, True)
-
+for gpu in tf.config.experimental.list_physical_devices('GPU') : tf.config.experimental.set_memory_growth(gpu, True)
 
 LABEL_MAP = 'dataset/label_map.pbtxt'
-PATH_TO_LABELS = 'dataset/labels/'
-IMAGE_PATHS = [file for file in os.listdir('dataset/images/resized_images') if file.endswith(('jpeg', 'png', 'jpg'))]
-PATH_TO_MODEL_DIR = 'models/exported_models/object_detection'
-PATH_TO_SAVED_MODEL = PATH_TO_MODEL_DIR + "/saved_model"
+PATH_TO_SAVED_MODEL = 'models/exported_models/object_detection/saved_model'
+IMAGE_SIZE = (1024,1024)
 
-def load_image_into_numpy_array(path): return np.array(Image.open(path))[:,:,:3]
+def object_detection(image_path):
+    if not os.path.exists('./out'): os.makedirs('./out')
+    image = load_image(image_path)
+    image_np = np.array(image)[:,:,:3]
+    image_tensor,scale_factor = preprocess_image(image)
+    model = load_model(PATH_TO_SAVED_MODEL)
+    print(f'Detecting objects in {image_path[-7:]}', end='')
+    detections = detect_objects(image_tensor,model)
+    detections['detection_boxes'] = detections['detection_boxes'] * scale_factor
+    visualize_detections(detections,image_np,'./out/' + image_path[-7:])
+    print('\tDone')
 
 
-print('Loading model...', end='')
-if not os.path.exists(PATH_TO_SAVED_MODEL):
-    print("\nError, no saved model found")
-    exit()
+def load_model(path):
+    print('Loading model...', end='')
+    start_time = time.time()
+    if not os.path.exists(path):
+        print("\nError, no saved model found")
+        exit()
+    model = tf.saved_model.load(path)
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print('Done! Took {} seconds'.format(elapsed_time))
+    return model
 
-start_time = time.time()
-# Load saved model and build the detection function
-detect_fn = tf.saved_model.load(PATH_TO_SAVED_MODEL)
-end_time = time.time()
-elapsed_time = end_time - start_time
-print('Done! Took {} seconds'.format(elapsed_time))
-category_index = label_map_util.create_category_index_from_labelmap(LABEL_MAP, use_display_name=True)
+def load_image(image_path):
+    return Image.open(image_path)
 
-if not os.path.exists('./out'): os.makedirs('./out')
-for image_path in IMAGE_PATHS:
-    print('Running inference for {}... '.format(image_path), end='')
-    image_np = load_image_into_numpy_array('dataset/images/resized_images/' + image_path)
+def preprocess_image(image):
+    square = expand_to_square(image)
+    resized_image = square.resize(IMAGE_SIZE,resample=Image.Resampling.LANCZOS)
+    scale_factor = square.size[0]/IMAGE_SIZE[0]
+    image_np = np.array(resized_image)[:,:,:3]
     input_tensor = tf.convert_to_tensor(image_np)   # The input needs to be a tensor, convert it using `tf.convert_to_tensor`.
     input_tensor = input_tensor[tf.newaxis, ...]    # The model expects a batch of images, so add an axis with `tf.newaxis`.
-    # input_tensor = np.expand_dims(image_np, 0)
+    return input_tensor,scale_factor
 
-    detections = detect_fn(input_tensor)
+def detect_objects(image_tensor,model):
+    detections = model(image_tensor)
     # All outputs are batches tensors.
     # Convert to numpy arrays, and take index [0] to remove the batch dimension.
     # We're only interested in the first num_detections.
@@ -58,21 +66,36 @@ for image_path in IMAGE_PATHS:
     detections = {key: value[0, :num_detections].numpy() for key, value in detections.items()}
     detections['num_detections'] = num_detections
     detections['detection_classes'] = detections['detection_classes'].astype(np.int64)  # detection_classes should be ints.
-    image_np_with_detections = image_np.copy()
+    return detections
 
+def visualize_detections(detections,image,out_path):
+    image_np_with_detections = image.copy()
+    category_index = label_map_util.create_category_index_from_labelmap(LABEL_MAP, use_display_name=True)
     viz_utils.visualize_boxes_and_labels_on_image_array(
-          image_np_with_detections,
-          detections['detection_boxes'],
-          detections['detection_classes'],
-          detections['detection_scores'],
-          category_index,
-          use_normalized_coordinates=True,
-          max_boxes_to_draw=200,
-          min_score_thresh=.30,
-          agnostic_mode=False)
+            image_np_with_detections,
+            detections['detection_boxes'],
+            detections['detection_classes'],
+            detections['detection_scores'],
+            category_index,
+            use_normalized_coordinates=True,
+            max_boxes_to_draw=200,
+            min_score_thresh=.30,
+            agnostic_mode=False)
 
     plt.figure()
     plt.imshow(image_np_with_detections)
-    plt.savefig('./out/'+image_path)
-    print('Done')
-plt.show()
+    plt.savefig('./out/'+out_path)
+    plt.show()
+
+def expand_to_square(image):
+    width, height = image.size
+    if width == height:
+        return image
+    elif width > height:
+        result = Image.new(image.mode, (width, width), (255,255,255))
+        result.paste(image)
+        return result
+    else:
+        result = Image.new(image.mode, (height, height), (255,255,255))
+        result.paste(image)
+        return result
